fd391ab7a9bc23aa5c9863a47277fdbae5045c4e
zhaojianxing <zhaojianxing@xiaomi.com>
2018-09-06 07:22:09 +0000

    remove some url

--- deviceTokenAuth/getRequestSign.sh
+++ deviceTokenAuth/getRequestSign.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-python profile.py -a 326766038739850240 -d "EVAL-DEVICE-ID"  -p PROFILE
+python profile.py -a 326766038739850240 -d "EVAL-DEVICE-ID"  -p PROFILE -P 326766038739850240-publickey.pem -K 326766038739850240-privatekey.pem
--- deviceTokenAuth/profile.py
+++ deviceTokenAuth/profile.py
@@ -6,24 +6,20 @@ from ecdsa import SigningKey, VerifyingKey
 from ecdsa.util import sigencode_der, sigdecode_der
 import wget
 import sys, getopt, os
-def generate_signature(app_id, device_id):
+def generate_signature(app_id, device_id, privatekey):
     """
       生成签名
     """
-    filename = "326766038739850240-privatekey.pem"
-    print "\n"
-    private_key = SigningKey.from_pem(open(filename).read())
+    private_key = SigningKey.from_pem(open(privatekey).read())
     signature = base64.urlsafe_b64encode(private_key.sign(device_id, hashfunc=hashlib.sha256, sigencode=sigencode_der))
     return signature
 
 
-def verify_signature(app_id, device_id, signature):
+def verify_signature(app_id, device_id, signature, publickey):
     """
-      验证签名. 服务端功能
+      验证签名 服务端实现
     """
-    filename = "326766038739850240-publickey.pem"
-    print "\n"
-    public_key = VerifyingKey.from_pem(open(filename).read())
+    public_key = VerifyingKey.from_pem(open(publickey).read())
     check_res = public_key.verify(base64.urlsafe_b64decode(signature), device_id,
                                   hashfunc=hashlib.sha256, sigdecode=sigdecode_der)
     return check_res
@@ -36,14 +32,15 @@ def main(argv):
   app_id = ''
   device_id = ''
   profile = ''
-  error_msg = 'profile.py -a <app_id> -d <device_id> -p <profile>'
+  error_msg = 'profile.py -a <app_id> -d <device_id> -p <profile> -P <publickey> -K <privatekey>'
   try:
-    opts, args = getopt.getopt(argv,"ha:d:p:")
+    opts, args = getopt.getopt(argv,"ha:d:p:P:K:")
   except getopt.GetoptError:
     print error_msg
+    print "Parse parms error"
     sys.exit(2)
 
-  if (len(opts) <= 1 or len(opts) >= 4):
+  if (len(opts) <= 1 or len(opts) >= 6):
     print error_msg
     sys.exit(1)
   
@@ -66,14 +63,30 @@ def main(argv):
     sys.exit(1)   
   profile = arg
   index = index + 1
+  opt, arg = opts[index]
+  if (opt != '-P'):
+    print error_msg
+    print "Get public key failed"
+    sys.exit(1)
+  publickey = arg
+  index = index + 1
+  opt, arg = opts[index]
+  if (opt != '-K'):
+    print error_msg
+    print "Get private key failed"
+    sys.exit(1)
+  privatekey = arg
+  index = index + 1
   print 'app_id:' + app_id
   print 'device_id：' + device_id
   print 'base64 deviceid:' + base64.b64encode(device_id)
   print 'profile：' + profile
-  sign_res = generate_signature(app_id, device_id)
+  print 'publickey: ' + publickey
+  print 'privatekey: ' + privatekey
+  sign_res = generate_signature(app_id, device_id, privatekey)
   print sign_res
   print "\n"
-  res = verify_signature(app_id, device_id, sign_res)
+  res = verify_signature(app_id, device_id, sign_res, publickey)
   print res
   print "\n"
   write_to_file(sign_res, profile)
@@ -81,8 +94,6 @@ def main(argv):
   '''
   PROFILR_URL "https://account.ai.xiaomi.com/thirdparty/auth/token"
   REFRESH_TOKEN_URL "https://account.ai.xiaomi.com/thirdparty/refresh/token"
-  PROFILE_STAGING_URL "http://account-staging.ai.xiaomi.com/thirdparty/auth/token"
-  REFRESH_TOKEN_STAGING_URL "http://account-staging.ai.xiaomi.com/thirdparty/refresh/token"
    
   '''
   params ='client_id='+app_id+'&device='+base64.b64encode(device_id)+'&sign='+sign_res
